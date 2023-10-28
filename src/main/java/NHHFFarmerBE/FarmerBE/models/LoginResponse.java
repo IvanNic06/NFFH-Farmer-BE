3be22f6066744ece97c93f8ca03c022fe51837b8
giovdellap <giovdellap@gmail.com>
2023-10-28 18:36:44 +0200

    Ao

--- src/main/java/NHHFFarmerBE/FarmerBE/models/LoginResponse.java
+++ src/main/java/NHHFFarmerBE/FarmerBE/models/LoginResponse.java
@@ -1,14 +1,47 @@
 package NHHFFarmerBE.FarmerBE.models;
 
+import NHHFFarmerBE.FarmerBE.entities.Farmer;
+
 public class LoginResponse {
     
     private String password;
     private boolean success;
+    private String id;
+    public String getId() {
+        return id;
+    }
 
 
-    public LoginResponse(String password){
-        this.password = password;
-        this.success = true;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    private String username;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public LoginResponse(Farmer farmer){
+        if (farmer != null) {
+            success = true;
+            id = String.valueOf(farmer.getId());
+            password = farmer.getPassword();
+            username = farmer.getUsername();
+        } else {
+            success = false;
+            password = "";
+            username = "";
+            id = "";
+        }
     }
 
 
--- src/main/java/NHHFFarmerBE/FarmerBE/models/SignupResponse.java
+++ src/main/java/NHHFFarmerBE/FarmerBE/models/SignupResponse.java
@@ -4,9 +4,21 @@ import NHHFFarmerBE.FarmerBE.entities.Farmer;
 
 public class SignupResponse {
     private boolean success;
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public SignupResponse(Farmer farmer) {
         success = (farmer != null);
+        if(farmer != null) {
+            this.id = String.valueOf(farmer.getId());
+        }
     }
 
     public boolean isSuccess() {
